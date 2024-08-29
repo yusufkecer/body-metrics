fb0966970977ea0369794000ccc4ea8dcda095e4
Yusuf Keçer <yusufkecer0007@gmail.com>
2024-08-30 01:35:20 +0300

    feat: Add lazy loading for images in onboard_model.dart

--- lib/feature/onboard/onboard_model.dart
+++ lib/feature/onboard/onboard_model.dart
@@ -1,8 +1,31 @@
 part of 'onboard.dart';
 
 mixin _OnboardModel on State<_OnboardBody>, _PageViewMixin {
-  void _skip() {
-    print(context.read<OnboardCubit>().state.introKey);
-    Locator.sl<OnboardCubit>().skip(_pageViewList.length - 1);
+  IntroKey pageController = IntroKey();
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<OnboardCubit>().updateIndex(0);
+    });
+    super.initState();
   }
+
+  @override
+  void dispose() {
+    pageController.currentState?.dispose();
+    super.dispose();
+  }
+
+  void _skip() {
+    context.read<OnboardCubit>().skip(_pageViewList.length - 1, pageController);
+  }
+
+  void _onDone() {}
+
+  void _onChange(int value) {
+    context.read<OnboardCubit>().updateIndex(value);
+  }
+
+  bool get _isEnd => context.watch<OnboardCubit>().state.currentPage != _pageViewList.length - 1;
 }
